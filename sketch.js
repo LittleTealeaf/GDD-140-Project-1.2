ff12c71dc19e75840414187adcdcf3baa836fa19
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-01-26 16:45:46 -0500

    Remove commented code

--- sketch.js
+++ sketch.js
@@ -2,29 +2,8 @@ function setup() {
   createCanvas(600, 600);
 }
 
-// var h = 0,m = 0,s = 0;
 
 var interpolate = 0.1;
-
-
-// function draw() {
-//   background(255);
-//   // Get current time and scale to their current degrees
-//   var offset = -PI / 2
-//   var c_hour = hour() * PI * 2 / 24 + offset;
-//   var c_minute = c_hour + minute() * PI * 2 / 60;
-//   var c_second = c_minute + second() * PI * 2 / 60;
-
-//   //interpolate the set h,m,s degrees to approach the goal values
-//   h = h + (c_hour - h) * interpolate;
-//   m = m + (c_minute - m) * interpolate;
-//   s = s + (c_second - s) * interpolate;
-  
-//   //print arcs using the goal values
-//   arc(300,300,500,500,offset,h)
-//   arc(300,300,400,400,h,m)
-//   arc(300,300,300,300,m,s)
-// }
 
 var disp = [0,0,0,0,0]
 
