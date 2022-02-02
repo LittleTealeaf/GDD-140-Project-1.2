5c3d16aecbde5122eb60c486cb63a8a0bcc00829
Thomas Kwashnak <shintaomonk@gmail.com>
2022-02-01 22:37:07 -0500

    Made strokes thicker

--- sketch.js
+++ sketch.js
@@ -2,6 +2,8 @@
 
 function setup() {
   createCanvas(600, 600);
+  
+  strokeWeight(10);
 }
 
 
@@ -10,14 +12,14 @@ var interp = 0.03;
 var disp = [0,0,0,0,0]
 
 function draw() {
-  background(255)
+  background(255);
 
   var target = [month() / 12, day() / 31, hour() / 24, minute() / 60, second() / 60]
 
   var start = -PI / 2;
   var end = 0;
 
-  var start_size = 500, decrease_size = 50;
+  var start_size = 550, decrease_size = 50;
 
   for(var i = 0; i < target.length; i++) {
     disp[i] += (target[i] * 2 * PI - disp[i]) * interp;
@@ -25,7 +27,7 @@ function draw() {
     end = start + disp[i];
     var size = start_size - decrease_size * i;
     if(start != end) {
-      arc(300,300,size,size,start,end)
+      arc(300,300,size,size,start,end);
     }
     start = end;
   }
